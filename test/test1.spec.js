c15d835c898db15ce4a7cdd049a8736406e6f36b
diinesh <hcl@10.29.95.75>
2016-06-27 01:26:01 -0500

    new repo

---
+++ test/test1.spec.js
@@ -0,0 +1,8 @@
+//var mocha=require()    
+var chai = require('chai');
+//var sinon=require('sinon');
+var expect = chai.expect;
+describe('1st tests', function () {
+    it('true is true', function () { expect(true).to.equal(true); });
+});
+//# sourceMappingURL=test1.spec.js.map
