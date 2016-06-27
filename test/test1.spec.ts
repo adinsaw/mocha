243ff2c5fcc30e7112833973bfcf516fc1cae449
adinsaw <adinsaw@gmail.com>
2016-06-27 01:52:49 -0500

    Update test1.spec.ts

--- test/test1.spec.ts
+++ test/test1.spec.ts
@@ -6,3 +6,6 @@ var expect=chai.expect;
 describe('1st tests', function() {
       it('true is true', function(){ expect(true).to.equal(true);});
     });
+describe('1st tests', function() {
+      it('true is true', function(){ expect(true).to.equal(true);});
+    });
